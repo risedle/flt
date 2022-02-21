6d7445206a9a2e5e8340c3c005f1cc574785f1d7
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-02-21 19:14:53 +0700

    ✦ First setup

--- src/FuseLeveragedToken.sol
+++ src/FuseLeveragedToken.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.11;
 
+
+/// @title Fuse Leveraged Token (FLT)
+/// @author github.com/pyk
+/// @notice Leveraged Token powered by Rari Fuse
 contract FuseLeveragedToken {
+    /// ███ Storages ███████████████████████████████████████████████████████████
+
+
+    /// ███ Constructors ███████████████████████████████████████████████████████
+
+    /// @notice Creates a new FLT that manages specific collateral and debt
     constructor() {
         return;
     }
