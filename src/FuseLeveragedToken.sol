a5434d9e3860436dafaedf52d813587e6cda5e33
pyk.eth ve(✦,✦) <bayualdiyansyah@gmail.com>
2022-02-21 20:43:45 +0700

    ✦ Add HEVM for testing (#11)
    * ✦ Update solc version
    
    * ✦ Add HEVM
    
    * ✦ Add build-and-test jobs
    
    * ✦ Update DAPP_REMAPPINGS
    
    * ✦ solhint: Ignore constructors

--- src/FuseLeveragedToken.sol
+++ src/FuseLeveragedToken.sol
@@ -1,18 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.11;
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
 
+import { ERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 /// @title Fuse Leveraged Token (FLT)
-/// @author github.com/pyk
+/// @author bayu (github.com/pyk)
 /// @notice Leveraged Token powered by Rari Fuse
-contract FuseLeveragedToken {
+contract FuseLeveragedToken is ERC20 {
     /// ███ Storages ███████████████████████████████████████████████████████████
 
+    /// @notice The ERC20 compliant token that used by FLT as collateral
+    address public collateral;
 
     /// ███ Constructors ███████████████████████████████████████████████████████
 
     /// @notice Creates a new FLT that manages specific collateral and debt
-    constructor() {
-        return;
+    /// @param _name The name of the Fuse Leveraged Token.
+    /// @param _symbol The symbol of the Fuse Leveraged Token.
+    /// @param _collateral The ERC20 compliant token the FLT accepts as collateral
+    constructor(string memory _name, string memory _symbol, address _collateral) ERC20(_name, _symbol) {
+        collateral = _collateral;
+    }
+
+    /// ███ User actions ███████████████████████████████████████████████████████
+
+    /// @notice Mints shares FLT shares to receiver by depositing exactly amount of collateral tokens.
+    /// @param _collateralAmount The amount of collateral
+    /// @param _recipient The address that will receive the minted leveraged tokens
+    /// @return _shares The amount of leveraged tokens minted
+    function deposit(uint256 _collateralAmount, address _recipient) external view returns (uint256 _shares) {
+        // TODO(pyk): Check max mint amount
+        return 0;
     }
 }
