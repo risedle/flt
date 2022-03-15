04c4d6d0715ad73d951b1339b730315c3f1dba92
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-16 01:48:58 +0700

    ✦ Update interfaces

--- src/test/SushiFlashSwap.t.sol
+++ src/test/SushiFlashSwap.t.sol
@@ -4,22 +4,31 @@ pragma experimental ABIEncoderV2;
 
 import "lib/ds-test/src/test.sol";
 
-import {IUniswapV2Factory} from "../interface/IUniswapV2Factory.sol";
-import {IUniswapV2Router02} from "../interface/IUniswapV2Router02.sol";
-import {IUniswapV2Pair} from "../interface/IUniswapV2Pair.sol";
+import {IUniswapV2Factory} from "../interfaces/IUniswapV2Factory.sol";
+import {IUniswapV2Router02} from "../interfaces/IUniswapV2Router02.sol";
+import {IUniswapV2Pair} from "../interfaces/IUniswapV2Pair.sol";
+import {IERC20} from "../interfaces/IERC20.sol";
 import {GOHM_ADDRESS} from "./Addresses.sol";
+import { HEVM } from "./HEVM.sol";
 
 contract SushiFlashSwap is DSTest {
 
-    function runFlashSwap() public {
+    IUniswapV2Router02 private router;
+    IUniswapV2Factory private factory;
+    address private WETH;
+
+    constructor() {
         // Sushiswap router
-        IUniswapV2Router02 router = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
+        router = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
         // Sushiswap Factory
-        IUniswapV2Factory factory = IUniswapV2Factory(router.factory());
+        factory = IUniswapV2Factory(router.factory());
         // WETH
-        address WETH_ADDRESS = router.WETH();
+        WETH = router.WETH();
+    }
+
+    function runFlashSwap() public {
         // Get pair address
-        address pairAddress = factory.getPair(GOHM_ADDRESS, WETH_ADDRESS);
+        address pairAddress = factory.getPair(GOHM_ADDRESS, WETH);
         // Get pair
         IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
 
@@ -36,22 +45,50 @@ contract SushiFlashSwap is DSTest {
         address borrowToken = GOHM_ADDRESS;
         uint256 amount0Out = borrowToken == token0 ? borrowAmount : 0;
         uint256 amount1Out = borrowToken == token1 ? borrowAmount : 0;
-        bytes memory data = abi.encode(borrowAmount, borrowToken);
+        bytes memory data = abi.encode(borrowAmount);
 
         // Perform the flashswap
         IUniswapV2Pair(pairAddress).swap(amount0Out, amount1Out, address(this), data);
 
     }
 
+    error NotAuthorized(address caller);
+
     // @notice Function is called by the Uniswap V2 pair's `swap` function
     function uniswapV2Call(address _sender, uint256 _amount0, uint256 _amount1, bytes calldata _data) external {
-        emit log_named_address("sender", _sender);
+        // Checks
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        if(msg.sender != factory.getPair(token0, token1)) revert NotAuthorized(msg.sender);
+        if(_sender != address(this)) revert NotAuthorized(_sender);
+
+        // Effects
+        uint256 borrowAmount = abi.decode(_data, (uint256));
+        uint256 fee = ((borrowAmount * 3) / 997) + 1;
+        uint256 repayAmount = borrowAmount + fee;
+
+        // Interaction
+        uint256 balance = IERC20(GOHM_ADDRESS).balanceOf(address(this));
+        emit log_named_uint("_amount0", _amount0);
+        emit log_named_uint("_amount1", _amount1);
+        emit log_named_uint("balance", balance);
+        emit log_named_uint("repayAmount", repayAmount);
+
+        //
     }
 }
 
 contract SushiFlashSwapTest is DSTest {
+    HEVM internal hevm;
+
+    function setUp() public {
+        hevm = new HEVM();
+    }
+
     function testRunFlashSwap() public {
         SushiFlashSwap flash = new SushiFlashSwap();
+        // Set USDC balance for the flashswap
+
         flash.runFlashSwap();
         assert(false);
     }
