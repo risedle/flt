4bcb9cb7ca2e6a5d7516389137e84d1f708c50b9
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-16 02:44:40 +0700

    ✦ interfaces: Add swapTokensForExactTokens to Uniswap Router

--- src/interfaces/IUniswapV2Router02.sol
+++ src/interfaces/IUniswapV2Router02.sol
@@ -4,5 +4,14 @@ pragma experimental ABIEncoderV2;
 
 interface IUniswapV2Router02 {
     function factory() external pure returns (address);
+
     function WETH() external pure returns (address);
+
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
 }
