b2faf652f5e491b2889009d631de53ea4051de25
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-16 22:13:35 +0700

    ✦ IfERC20: add accrualBlockNumber

--- src/interfaces/IfERC20.sol
+++ src/interfaces/IfERC20.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.8.11;
 pragma experimental ABIEncoderV2;
 
-import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
-interface IfERC20 is IERC20 {
+interface IfERC20 {
     function mint(uint256 mintAmount) external returns (uint256);
 
     function redeem(uint256 redeemTokens) external returns (uint256);
@@ -12,4 +10,6 @@ interface IfERC20 is IERC20 {
     function borrow(uint256 borrowAmount) external returns (uint256);
 
     function repayBorrow(uint256 repayAmount) external returns (uint256);
+
+    function accrualBlockNumber() external returns (uint256);
 }
