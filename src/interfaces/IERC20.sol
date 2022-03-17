e1f467e54ae5adfde5269639f1d2224e49f653e2
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-18 03:00:50 +0700

    ✦ interfaces: Remove IERC20

--- src/interfaces/IERC20.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.11;
-pragma experimental ABIEncoderV2;
-
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-}
