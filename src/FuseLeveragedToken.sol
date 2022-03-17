47e8fdf9e4cbbca769e9c875288510684ac399d4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-17 18:55:13 +0700

    ✦ flt: Implement and test setMaxDeposit

--- src/FuseLeveragedToken.sol
+++ src/FuseLeveragedToken.sol
@@ -3,42 +3,72 @@ pragma solidity 0.8.11;
 pragma experimental ABIEncoderV2;
 
 import { ERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-/// @title Fuse Leveraged Token (FLT)
-/// @author bayu (github.com/pyk)
-/// @notice Leveraged Token powered by Rari Fuse
-contract FuseLeveragedToken is ERC20 {
+/**
+ * @title Fuse Leveraged Token (FLT)
+ * @author bayu (github.com/pyk)
+ * @notice Leveraged Token powered by Rari Fuse
+ */
+contract FuseLeveragedToken is ERC20, Ownable {
     /// ███ Storages ███████████████████████████████████████████████████████████
 
     /// @notice The ERC20 compliant token that used by FLT as collateral
-    address public collateral;
+    address public immutable collateral;
+
+    /**
+     * @notice The maximum amount of the collateral token that can be deposited
+     *         into the FLT contract through deposit function.
+     */
+    uint256 public maxDeposit;
+
+    /// ███ Events █████████████████████████████████████████████████████████████
+
+    /// @notice Event emitted when maxDeposit is updated
+    event MaxDepositUpdated(uint256 newMaxDeposit);
 
     /// ███ Errors █████████████████████████████████████████████████████████████
 
-    error DepositMaxedOut();
+    error DepositAmountTooLarge(uint256 amount);
+    error DepositAmountCannotBeZero();
+    error MaxDepositAmountCannotBeZero();
+
     error RecipientZeroAddress();
 
     /// ███ Constructors ███████████████████████████████████████████████████████
 
-    /// @notice Creates a new FLT that manages specific collateral and debt
-    /// @param _name The name of the Fuse Leveraged Token.
-    /// @param _symbol The symbol of the Fuse Leveraged Token.
-    /// @param _collateral The ERC20 compliant token the FLT accepts as collateral
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address _collateral
-    ) ERC20(_name, _symbol) {
+    /**
+     * @notice Creates a new FLT that manages specified collateral and debt
+     * @param _name The name of the Fuse Leveraged Token (e.g. gOHM 2x Long)
+     * @param _symbol The symbol of the Fuse Leveraged Token (e.g. gOHMRISE)
+     * @param _collateral The ERC20 compliant token the FLT accepts as collateral
+     */
+    constructor(string memory _name, string memory _symbol, address _collateral) ERC20(_name, _symbol) {
         collateral = _collateral;
+    }
+
+    /// ███ Owner actions ██████████████████████████████████████████████████████
+
+    /**
+     * @notice Set the maxDeposit
+     * @param _newMaxDeposit New maximum deposit
+     */
+    function setMaxDeposit(uint256 _newMaxDeposit) external onlyOwner {
+        if (_newMaxDeposit == 0) revert MaxDepositAmountCannotBeZero();
+        maxDeposit = _newMaxDeposit;
+        emit MaxDepositUpdated(_newMaxDeposit);
     }
 
     /// ███ User actions ███████████████████████████████████████████████████████
 
-    /// @notice Mints shares FLT shares to receiver by depositing exactly amount of collateral tokens.
-    /// @param _collateralAmount The amount of collateral
-    /// @param _recipient The address that will receive the minted leveraged tokens
-    /// @return _shares The amount of leveraged tokens minted
-    function deposit(uint256 _collateralAmount, address _recipient) external view returns (uint256 _shares) {
+    /**
+     * @notice Mints `_shares` amount of FLT token to `_recipient` by depositing
+     *         exactly `_amount` amount of collateral tokens.
+     * @param _amount The amount of collateral
+     * @param _recipient The address that will receive the minted FLT token
+     * @return _shares The amount of minted FLT tokens
+     */
+    function deposit(uint256 _amount, address _recipient) external view returns (uint256 _shares) {
         // TODO(pyk): Check max mint amount
 
         return 0;
---
+++ src/test/FuseLeveragedTokenAccessControl.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+import "lib/ds-test/src/test.sol";
+
+import { FuseLeveragedToken } from "../FuseLeveragedToken.sol";
+import { HEVM } from "./HEVM.sol";
+import {gohm} from "./Arbitrum.sol";
+
+/**
+ * @title Fuse Leveraged Token Access Control Test
+ * @author bayu (github.com/pyk)
+ * @notice Make sure the access control works as expected
+ */
+contract FuseLeveragedTokenAccessControlTest is DSTest {
+    HEVM private hevm;
+
+    function setUp() public {
+        hevm = new HEVM();
+    }
+
+    /// @notice Make sure non-owner cannot set the maxDeposit value
+    function testFailNonOwnerCannotSetMaxDeposit() public {
+        // Create new FLT; by default the deployer is the owner
+        FuseLeveragedToken flt = new FuseLeveragedToken("gOHM 2x Long", "gOHMRISE", gohm);
+
+        // Transfer the ownership
+        address newOwner = hevm.addr(1);
+        flt.transferOwnership(newOwner);
+
+        // Non-owner trying to set the maxDeposit value
+        flt.setMaxDeposit(1 ether); // This should be failed
+    }
+}
