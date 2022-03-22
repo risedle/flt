751e9d4bfa7ebc752f4d76be725d90a47f598d58
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-23 01:12:25 +0700

    ✦ IUniswapV2Router02: Add getAmountsOut

--- src/interfaces/IUniswapV2Router02.sol
+++ src/interfaces/IUniswapV2Router02.sol
@@ -4,9 +4,7 @@ pragma experimental ABIEncoderV2;
 
 interface IUniswapV2Router02 {
     function factory() external pure returns (address);
-
     function WETH() external pure returns (address);
-
     function swapTokensForExactTokens(
         uint256 amountOut,
         uint256 amountInMax,
@@ -14,6 +12,6 @@ interface IUniswapV2Router02 {
         address to,
         uint256 deadline
     ) external returns (uint256[] memory amounts);
-
     function getAmountsIn(uint256 amountOut, address[] memory path) external view returns (uint256[] memory amounts);
+    function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
 }
