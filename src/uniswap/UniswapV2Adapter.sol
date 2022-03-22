523228ee9db4a59c0a075db2dc330e00673ca3e4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-23 01:13:29 +0700

    ✦ UniswapV2Adapter: Refactor

--- src/uniswap/UniswapV2Adapter.sol
+++ src/uniswap/UniswapV2Adapter.sol
@@ -52,7 +52,7 @@ contract UniswapV2Adapter {
 
     /// ███ Internal functions █████████████████████████████████████████████████
 
-    function onFlashSwapExactTokensForTokensViaETH(uint256 _wethAmount, uint256 _amountOut, address[2] calldata _tokens, address _flasher, bytes calldata _data) internal {
+    function onFlashSwapExactTokensForTokensViaETH(uint256 _wethAmount, uint256 _amountIn, uint256 _amountOut, address[2] memory _tokens, address _flasher, bytes memory _data) internal {
         /// ███ Checks
 
         // Check pairs
@@ -67,7 +67,7 @@ contract UniswapV2Adapter {
         address token1 = IUniswapV2Pair(tokenOutPair).token1();
         uint256 amount0Out = _tokens[1] == token0 ? _amountOut : 0;
         uint256 amount1Out = _tokens[1] == token1 ? _amountOut : 0;
-        IERC20(weth).safeTransfer(tokenOutPair, wethAmount);
+        IERC20(weth).safeTransfer(tokenOutPair, _wethAmount);
         IUniswapV2Pair(tokenOutPair).swap(amount0Out, amount1Out, address(this), bytes(""));
 
         // Step 5:
@@ -76,12 +76,20 @@ contract UniswapV2Adapter {
 
         // Step 6:
         // Call the flasher
-        IFlashSwapper(flasher).onFlashSwapExactTokensForTokensViaETH(_amountOut, _data);
+        IFlashSwapper(_flasher).onFlashSwapExactTokensForTokensViaETH(_amountOut, _data);
 
         // Step 8:
         // Repay the flashswap
         IERC20(_tokens[0]).safeTransfer(tokenInPair, _amountIn);
 
+    }
+
+    function getAmountOutViaETH(address[2] memory _tokens, uint256 _amountIn) internal returns (uint256 _amountOut) {
+        address[] memory tokenInToTokenOut = new address[](3);
+        tokenInToTokenOut[0] = _tokens[0];
+        tokenInToTokenOut[1] = weth;
+        tokenInToTokenOut[3] = _tokens[1];
+        _amountOut = IUniswapV2Router02(router).getAmountsOut(_amountIn, tokenInToTokenOut)[2];
     }
 
 
@@ -92,7 +100,9 @@ contract UniswapV2Adapter {
         /// ███ Checks
 
         // Check caller
-        if (msg.sender != repayPair) revert FlashSwapNotAuthorized();
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        if (msg.sender != IUniswapV2Factory(IUniswapV2Router02(router).factory()).getPair(token0, token1)) revert FlashSwapNotAuthorized();
         if (_sender != address(this)) revert FlashSwapNotAuthorized();
 
         // Get the data
@@ -102,8 +112,8 @@ contract UniswapV2Adapter {
         if (flashSwapType == FlashSwapType.FlashSwapExactTokensForTokensViaETH) {
             // Get WETH amount
             uint256 wethAmount = _amount0 == 0 ? _amount1 : _amount0;
-            (uint256 amountOut, address tokenIn, address tokenOut, address flasher, bytes memory callData) = abi.decode(data, (uint256,address,address,address,bytes));
-            onFlashSwapExactTokensForTokensViaETH(wethAmount, amountOut, [tokenIn, tokenOut], flasher, callData);
+            (uint256 amountIn, uint256 amountOut, address tokenIn, address tokenOut, address flasher, bytes memory callData) = abi.decode(data, (uint256,uint256,address,address,address,bytes));
+            onFlashSwapExactTokensForTokensViaETH(wethAmount, amountIn, amountOut, [tokenIn, tokenOut], flasher, callData);
             return;
         }
     }
@@ -126,12 +136,10 @@ contract UniswapV2Adapter {
 
         // Check pairs
         address tokenInPair = IUniswapV2Factory(IUniswapV2Router02(router).factory()).getPair(_tokens[0], weth);
-        address tokenOutPair = IUniswapV2Factory(IUniswapV2Router02(router).factory()).getPair(_tokens[1], weth);
         if (tokenInPair == address(0)) revert FlashSwapPairNotFound(_tokens[0], weth);
-        if (tokenOutPair == address(0)) revert FlashSwapPairNotFound(_tokens[1], weth);
 
         // Check the amount of tokenOut
-        uint256 amountOut = IUniswapV2Router02(router).getAmountsOut(_amountIn, [_tokens[0], weth, _tokens[1]]);
+        uint256 amountOut = getAmountOutViaETH(_tokens, _amountIn);
         if (amountOut > _amountOutMin) revert FlashSwapAmountOutTooLow(_amountOutMin, amountOut);
 
         /// ███ Effects
@@ -140,18 +148,20 @@ contract UniswapV2Adapter {
 
         // Step 1:
         // Calculate how much WETH we need to borrow from tokenIn/WETH pair
-        uint256 wethAmount = IUniswapV2Router02(router).getAmountsIn(_amount, [weth, amountOut])[0];
+        address[] memory wethToTokenOut = new address[](2);
+        wethToTokenOut[0] = weth;
+        wethToTokenOut[1] = _tokens[1];
+        uint256 wethAmount = IUniswapV2Router02(router).getAmountsIn(amountOut, wethToTokenOut)[0];
 
         // Step 2:
         // Borrow WETH from tokenIn/WETH liquidity pair (e.g. USDC/WETH)
-        address token0 = IUniswapV2Pair(tokenInPair).token0();
-        address token1 = IUniswapV2Pair(tokenInPair).token1();
-        uint256 amount0Out = weth == token0 ? wethAmount : 0;
-        uint256 amount1Out = weth == token1 ? wethAmount : 0;
+        uint256 amount0Out = weth == IUniswapV2Pair(tokenInPair).token0() ? wethAmount : 0;
+        uint256 amount1Out = weth == IUniswapV2Pair(tokenInPair).token1() ? wethAmount : 0;
 
         // Step 3:
         // Perform the flashswap to Uniswap V2; Step 4 in onFlashSwapExactTokensForTokensViaETH
-        bytes memory data = abi.encode(FlashSwapTtype.FlashSwapExactTokensForTokensViaETH, abi.encode(amountOut, _tokens[0], _tokens[1], msg.sender, _data));
+        bytes memory data = abi.encode(FlashSwapType.FlashSwapExactTokensForTokensViaETH, abi.encode(_amountIn, amountOut, _tokens[0], _tokens[1], msg.sender, _data));
         IUniswapV2Pair(tokenInPair).swap(amount0Out, amount1Out, address(this), data);
+
     }
 }
