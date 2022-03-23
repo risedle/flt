9dd8cf2aad3ace480413c9130ca7066017cec541
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-23 19:49:05 +0700

    ✦ interfaces: Add IFuseComptroller

---
+++ src/interfaces/IFuseComptroller.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @title Rari Fuse Comptroller Interface
+ * @author bayu (github.com/pyk)
+ * @dev docs: https://docs.rari.capital/fuse/#comptroller
+ */
+interface IFuseComptroller {
+    function getAccountLiquidity(address account) external returns (uint256 error, uint256 liquidity, uint256 shortfall);
+    function enterMarkets(address[] calldata fTokens) external returns (uint[] memory);
+}
