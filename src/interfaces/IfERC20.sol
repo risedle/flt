4688f8285fc39f76696c23cc1ae22046d92070f7
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-23 19:49:43 +0700

    ✦ IfERC20: Add borrowBalanceCurrent & comptroller

--- src/interfaces/IfERC20.sol
+++ src/interfaces/IfERC20.sol
@@ -2,14 +2,17 @@
 pragma solidity 0.8.11;
 pragma experimental ABIEncoderV2;
 
+/**
+ * @title Rari Fuse ERC20 Interface
+ * @author bayu (github.com/pyk)
+ * @dev docs: https://docs.rari.capital/fuse/#ftoken-s
+ */
 interface IfERC20 {
     function mint(uint256 mintAmount) external returns (uint256);
-
     function redeem(uint256 redeemTokens) external returns (uint256);
-
     function borrow(uint256 borrowAmount) external returns (uint256);
-
     function repayBorrow(uint256 repayAmount) external returns (uint256);
-
     function accrualBlockNumber() external returns (uint256);
+    function borrowBalanceCurrent(address account) external returns (uint256);
+    function comptroller() external returns (address);
 }
