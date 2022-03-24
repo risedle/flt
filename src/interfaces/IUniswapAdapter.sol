9535dc9579ff7d2ce4aa54a70bc64762ea8c0635
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-25 02:38:02 +0700

    ✦ IUniswapAdapter: Add getAmountInViaETH & swapTokensForExactTokensViaETH

--- src/interfaces/IUniswapAdapter.sol
+++ src/interfaces/IUniswapAdapter.sol
@@ -10,4 +10,6 @@ pragma experimental ABIEncoderV2;
 interface IUniswapAdapter {
     function flashSwapExactTokensForTokensViaETH(uint256 _amountIn, uint256 _amountOutMin, address[2] calldata _tokens, bytes calldata _data) external;
     function getAmountOutViaETH(address[2] memory _tokens, uint256 _amountIn) external view returns (uint256 _amountOut);
+    function getAmountInViaETH(address[2] memory _tokens, uint256 _amountOut) external view returns (uint256 _amountIn);
+    function swapTokensForExactTokensViaETH(uint256 _amountOut, uint256 _amountInMax, address[2] calldata _tokens) external returns (uint256 _amountIn);
 }
