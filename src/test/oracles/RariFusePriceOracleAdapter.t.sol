f6895f9d15c9186ac291977dc63c0e56784e4a5e
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-27 06:39:47 +0700

    ✦ tests: Add RariFusePriceOracleAdapterTest

---
+++ src/test/oracles/RariFusePriceOracleAdapter.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+import "lib/ds-test/src/test.sol";
+
+import { HEVM } from "../hevm/HEVM.sol";
+import { RariFusePriceOracleAdapter } from "../../oracles/RariFusePriceOracleAdapter.sol";
+import { gohm, rariFuseGOHMPriceOracle } from "chain/Tokens.sol";
+import { usdc, rariFuseUSDCPriceOracle } from "chain/Tokens.sol";
+
+/**
+ * @title Rari Fuse Price Oracle Adapter Test
+ * @author bayu <bayu@risedle.com> <github.com/pyk>
+ */
+contract RariFusePriceOracleAdapterTest is DSTest {
+    HEVM private hevm;
+
+    function setUp() public {
+        hevm = new HEVM();
+    }
+
+    /// @notice Make sure non-owner can set the oracle
+    function testFailNonOwnerCannotSetOracle() public {
+        // Create new oracle
+        RariFusePriceOracleAdapter oracle = new RariFusePriceOracleAdapter();
+
+        // Transfer ownership
+        address newOwner = hevm.addr(1);
+        oracle.transferOwnership(newOwner);
+
+        // Set oracle for token
+        oracle.setOracle(gohm, rariFuseGOHMPriceOracle);
+    }
+}
