78b4378e174c4e4fc8f35050d4cf8103f3a29111
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-26 20:50:12 +0700

    ✦ ethereum: Add WBTC on Tokens

--- src/test/ethereum/Tokens.sol
+++ src/test/ethereum/Tokens.sol
@@ -6,6 +6,7 @@ pragma experimental ABIEncoderV2;
 address constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 address constant gohm = 0x0ab87046fBb341D058F17CBC4c1133F25a20a52f;
 address constant usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+address constant wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
 
 // fToken on Risedle Fuse Pools
 // https://rari.app/fuse/pool/177
@@ -17,6 +18,10 @@ address constant feth  = 0x340d64fBf1EE1ffb3Fe022746fB97a598F3d92A9;
 uint256 constant wethSlot = 3;
 uint256 constant usdcSlot = 9;
 uint256 constant gohmSlot = 0;
+uint256 constant wbtcSlot = 0;
 
-// Sushiswap
-address constant sushiRouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
+// Uniswap Routers, Pools and Pairs
+address constant uniswapV2Router      = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+address constant uniswapV3Router      = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+address constant uniswapV3WBTCETHPool = 0xCBCdF9626bC03E24f779434178A73a0B4bad62eD;
+address constant sushiRouter          = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
