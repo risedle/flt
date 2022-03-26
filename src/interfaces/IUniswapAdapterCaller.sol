d07c5942f4e433e2e6d67ba7a5834f0db88904dd
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-26 20:47:40 +0700

    ✦ IFlashSwapper: Rename to IUniswapAdapterCaller

---
+++ src/interfaces/IUniswapAdapterCaller.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @title Uniswap Adapter Caller Interface
+ * @author bayu (github.com/pyk)
+ * @notice Contract that interact with Uniswap Adapter should implement this interface.
+ */
+interface IUniswapAdapterCaller {
+    /**
+     * @notice Function that will be executed by Uniswap Adapter to finish the flash swap.
+     *         The caller will receive _amountOut of the specified tokenOut.
+     * @param _wethAmount The amount of WETH that the caller need to send back to the Uniswap Adapter
+     * @param _amountOut The amount of of tokenOut transfered to the caller.
+     * @param _data Data passed by the caller.
+     */
+    function onFlashSwapETHForExactTokens(uint256 _wethAmount, uint256 _amountOut, bytes calldata _data) external;
+}
