2517c3794f3639cfc22e1f60ea71da810ba58799
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-26 21:55:15 +0700

    ✦ UniswapAdapterTest: Test flashSwapETHForExactTokens on Uniswap V2

--- src/test/uniswap/UniswapAdapter.t.sol
+++ src/test/uniswap/UniswapAdapter.t.sol
@@ -6,7 +6,9 @@ import "lib/ds-test/src/test.sol";
 import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import { weth, wbtc, uniswapV3WBTCETHPool, uniswapV3Router } from "chain/Tokens.sol";
+import { weth, wbtc } from "chain/Tokens.sol";
+import { uniswapV3WBTCETHPool, uniswapV3Router } from "chain/Tokens.sol";
+import { sushiRouter, sushiWBTCETHPair } from "chain/Tokens.sol";
 import { UniswapAdapter } from "../../uniswap/UniswapAdapter.sol";
 import { HEVM } from "../hevm/HEVM.sol";
 import { IUniswapV2Pair } from "../../interfaces/IUniswapV2Pair.sol";
@@ -18,6 +20,8 @@ import { IUniswapV3Pool } from "../../interfaces/IUniswapV3Pool.sol";
  * @notice Unit testing for Uniswap Adapter
  */
 contract UniswapAdapterTest is DSTest {
+
+    using SafeERC20 for IERC20;
     HEVM private hevm;
 
     function setUp() public {
@@ -55,4 +59,51 @@ contract UniswapAdapterTest is DSTest {
         assertEq(router, uniswapV3Router);
     }
 
+    enum TestType { CallerRepay, CallerNotRepay }
+
+    /// @notice Make sure flashSwapETHForExactTokens is working on Uniswap V2
+    function testUniswapV2FlashSwapETHForExactTokens() public {
+        // Create new Uniswap Adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Owner set metadata for wbtc
+        adapter.setMetadata(wbtc, 2, sushiWBTCETHPair, sushiRouter);
+
+        // Execute the flashswap
+        bytes memory data = abi.encode(TestType.CallerRepay, 2022);
+        adapter.flashSwapETHForExactTokens(wbtc, 1e8, data);
+    }
+
+    /// @notice Make sure flashSwapETHForExactTokens is failed when token is not repay
+    function testFailedUniswapV2FlashSwapETHForExactTokensRevertedIfCallerNotRepay() public {
+        // Create new Uniswap Adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Owner set metadata for wbtc
+        adapter.setMetadata(wbtc, 2, sushiWBTCETHPair, sushiRouter);
+
+        // Execute the flashswap without repay; this should be reverted
+        bytes memory data = abi.encode(TestType.CallerNotRepay, 2022);
+        adapter.flashSwapETHForExactTokens(wbtc, 1e8, data);
+    }
+
+    /// @notice Check the callback
+    function onFlashSwapETHForExactTokens(uint256 _wethAmount, uint256 _amountOut, bytes memory _data) external {
+        assertGt(_wethAmount, 0, "check _wethAmount");
+        assertGt(_amountOut, 0, "check _amountOut");
+        assertEq(IERC20(wbtc).balanceOf(address(this)), _amountOut, "check balance");
+
+        // Check passed data
+        (TestType testType, uint256 pin) = abi.decode(_data, (TestType, uint256));
+        assertEq(pin, 2022, "check pin");
+        if (testType == TestType.CallerRepay) {
+            hevm.setWETHBalance(address(this), _wethAmount);
+            IERC20(weth).safeTransfer(msg.sender, _wethAmount);
+            return;
+        }
+        if (testType == TestType.CallerNotRepay) {
+            // Do nothing; this should be reverted
+            return;
+        }
+    }
 }
