5de692149c0ea292b3943817e1ae87331e2ecf15
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-27 06:38:54 +0700

    ✦ interfaces: Add IRariFusePriceOracle

---
+++ src/interfaces/IRariFusePriceOracle.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @title Rari Fuse Price Oracle Interface
+ * @author bayu <bayu@risedle.com> <https://github.com/pyk>
+ */
+interface IRariFusePriceOracle {
+    /**
+     * @notice Gets the price in ETH of `_token`
+     * @param _token ERC20 token address
+     * @return _price Price in 1e18 precision
+     */
+    function price(address _token) external view returns (uint256 _price);
+}
