a67c65644fead68700c15571eb4398b9a09957fb
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-27 14:42:12 +0700

    ✦ chain: Add more fToken addresses

--- src/test/arbitrum/Tokens.sol
+++ src/test/arbitrum/Tokens.sol
@@ -11,10 +11,12 @@ address constant wbtc = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
 // Rari Fuse
 address constant fgohm = 0xd861026A12623aec769fA57D05201193D8844368;
 address constant fusdc = 0x156157693BA371c5B126AAeF784D2853EbC8fEFa;
+address constant fwbtc = 0xC890044a3e44cbb98fC12C87012a5e50E7A3aD3d;
 
 // Rari Fuse Price Oracles
 address constant rariFuseGOHMPriceOracle = 0x5dA877E48662d14379C6F425f1de8E69Ad01C527;
 address constant rariFuseUSDCPriceOracle = 0x5dA877E48662d14379C6F425f1de8E69Ad01C527;
+address constant rariFuseWBTCPriceOracle = 0x5dA877E48662d14379C6F425f1de8E69Ad01C527;
 
 // ERC20 slots on Arbitrum to manipulate the token balance
 uint256 constant usdcSlot = 51;
@@ -26,5 +28,6 @@ uint256 constant wbtcSlot = 51;
 address constant uniswapV2Router      = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506; // Use SushiSwap as Univ2 on arbitrum
 address constant uniswapV3Router      = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
 address constant uniswapV3WBTCETHPool = 0x149e36E72726e0BceA5c59d40df2c43F60f5A22D;
+address constant uniswapV3USDCETHPool = 0xC31E54c7a869B9FcBEcc14363CF510d1c41fa443;
 address constant sushiRouter          = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
 address constant sushiWBTCETHPair     = 0x515e252b2b5c22b4b2b6Df66c2eBeeA871AA4d69;
--- src/test/ethereum/Tokens.sol
+++ src/test/ethereum/Tokens.sol
@@ -13,10 +13,12 @@ address constant wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
 address constant fgohm = 0x720424E9cB93F46585C902512cC4DA2E8A06c86C;
 address constant fusdc = 0xDE35E22Ac73d088BB9e7Cf29F306C008B3Dc8a21;
 address constant feth  = 0x340d64fBf1EE1ffb3Fe022746fB97a598F3d92A9;
+address constant fwbtc = 0x2f17290A0C65e5d868427f473C38Ee9d13851e97;
 
 // Rari Fuse Price Oracles
 address constant rariFuseGOHMPriceOracle = 0x2111294D1A3889bC9d78bbEdC3Dbf5b6FA661612;
 address constant rariFuseUSDCPriceOracle = 0x058c345D3240001088b6280e008F9e78b3B2112d;
+address constant rariFuseWBTCPriceOracle = 0x2111294D1A3889bC9d78bbEdC3Dbf5b6FA661612;
 
 // ERC20 slots on Arbitrum to manipulate the token balance
 uint256 constant wethSlot = 3;
@@ -28,5 +30,6 @@ uint256 constant wbtcSlot = 0;
 address constant uniswapV2Router      = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 address constant uniswapV3Router      = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
 address constant uniswapV3WBTCETHPool = 0xCBCdF9626bC03E24f779434178A73a0B4bad62eD;
+address constant uniswapV3USDCETHPool = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
 address constant sushiRouter          = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
 address constant sushiWBTCETHPair     = 0xCEfF51756c56CeFFCA006cD410B03FFC46dd3a58;
