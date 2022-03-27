14284ee3e673dda4f073c548e487488d9310bef2
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-27 20:23:54 +0700

    ✦ IUniswapAdapter: Add swapExactTokensForWETH

--- src/interfaces/IUniswapAdapter.sol
+++ src/interfaces/IUniswapAdapter.sol
@@ -9,4 +9,6 @@ pragma experimental ABIEncoderV2;
  */
 interface IUniswapAdapter {
     function isConfigured(address _token) external returns (bool);
+    function flashSwapETHForExactTokens(address _tokenOut, uint256 _amountOut, bytes memory _data) external;
+    function swapExactTokensForWETH(address _tokenIn, uint256 _amountIn, uint256 _amountOutMin) external returns (uint256 _amountOut);
 }
