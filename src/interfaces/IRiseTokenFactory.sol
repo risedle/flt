01894a9b061cd66fd32e207a21a7f37ee1ec46f3
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-27 13:11:35 +0700

    ✦ interfaces: Add IRiseTokenFactory

---
+++ src/interfaces/IRiseTokenFactory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @title Rise Token Factory Interface
+ * @author bayu <bayu@risedle.com> <https://github.com/pyk>
+ */
+interface IRiseTokenFactory {
+    function uniswapAdapter() external returns (address);
+    function oracleAdapter() external returns (address);
+    function feeRecipient() external returns (address);
+}
