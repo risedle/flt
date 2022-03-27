757efc39a19456f2e9dc2f7c8919ccf5d0a72e13
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-27 11:28:10 +0700

    ✦ IUniswapAdapter: Update interface

--- src/interfaces/IUniswapAdapter.sol
+++ src/interfaces/IUniswapAdapter.sol
@@ -8,8 +8,5 @@ pragma experimental ABIEncoderV2;
  * @notice One interface to interact with Uniswap V2 and Uniswap V3
  */
 interface IUniswapAdapter {
-    function flashSwapExactTokensForTokensViaETH(uint256 _amountIn, uint256 _amountOutMin, address[2] calldata _tokens, bytes calldata _data) external;
-    function getAmountOutViaETH(address[2] memory _tokens, uint256 _amountIn) external view returns (uint256 _amountOut);
-    function getAmountInViaETH(address[2] memory _tokens, uint256 _amountOut) external view returns (uint256 _amountIn);
-    function swapTokensForExactTokensViaETH(uint256 _amountOut, uint256 _amountInMax, address[2] calldata _tokens) external returns (uint256 _amountIn);
+    function isConfigured(address _token) external returns (bool);
 }
