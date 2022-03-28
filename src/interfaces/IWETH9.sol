117d050dfc8ffdef08715c958c8ba47013234fb0
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-28 22:56:33 +0700

    ✦ interfaces: Add IWETH9

---
+++ src/interfaces/IWETH9.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.11;
+pragma experimental ABIEncoderV2;
+
+import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title WETH Interface
+ * @author bayu <bayu@risedle.com> <https://github.com/pyk>
+ */
+interface IWETH9 is IERC20 {
+    /// @notice Deposit ether to get wrapped ether
+    function deposit() external payable;
+
+    /// @notice Withdraw wrapped ether to get ether
+    function withdraw(uint256) external;
+}
