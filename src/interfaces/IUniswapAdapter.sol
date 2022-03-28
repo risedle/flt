0849196df6bc3d3a009d30f3e230d6fa3438bca3
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-03-28 22:56:03 +0700

    ✦ IUniswapAdapter: Add flashSwapWETHForExactTokens & weth

--- src/interfaces/IUniswapAdapter.sol
+++ src/interfaces/IUniswapAdapter.sol
@@ -8,11 +8,12 @@ pragma experimental ABIEncoderV2;
  * @notice One interface to interact with Uniswap V2 and Uniswap V3
  */
 interface IUniswapAdapter {
-    function isConfigured(address _token) external returns (bool);
-    function flashSwapETHForExactTokens(address _tokenOut, uint256 _amountOut, bytes memory _data) external;
+    function isConfigured(address _token) external view returns (bool);
+    function flashSwapWETHForExactTokens(address _tokenOut, uint256 _amountOut, bytes memory _data) external;
     function swapExactTokensForWETH(address _tokenIn, uint256 _amountIn, uint256 _amountOutMin) external returns (uint256 _amountOut);
     function previewSwapWETHForExactTokens(address _tokenOut, uint256 _amountOut) external view returns (uint256 _wethAmount);
     function previewSwapTokensForExactWETH(address _tokenIn, uint256 _wethAmount) external view returns (uint256 _amountIn);
     function previewSwapExactTokensForWETH(address _tokenIn, uint256 _amountIn) external view returns (uint256 _wethAmount);
     function previewSwapExactWETHForTokens(address _tokenOut, uint256 _wethAmount) external view returns (uint256 _amountOut);
+    function weth() external view returns (address);
 }
