b9d5ea621b996727b34aa96e131cf5136f42e039
pyk <bayualdiyansyah@gmail.com>
2022-04-30 10:27:10 +0700

    ✦ [ABDK 2] Remove unused experimental pragma (#16)

--- src/RiseTokenFactory.sol
+++ src/RiseTokenFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
+
 
 import { Ownable } from "openzeppelin/access/Ownable.sol";
 import { IERC20Metadata } from "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
--- src/interfaces/IRariFusePriceOracleAdapter.sol
+++ src/interfaces/IRariFusePriceOracleAdapter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
+
 
 import { IRariFusePriceOracle } from "./IRariFusePriceOracle.sol";
 
