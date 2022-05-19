006a8baee080f9a5617403a0e45a31c147a63ac2
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-05-19 15:00:05 +0700

    Refactor flashSwapWETHForExactTokens test

--- src/test/adapters/UniswapAdapterTest.sol
+++ src/test/adapters/UniswapAdapterTest.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.0;
 
 import { IVM } from "../IVM.sol";
+import { VMUtils } from "../VMUtils.sol";
+
+import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
 
 import { weth, wbtc } from "chain/Tokens.sol";
 import { uniswapV3WBTCETHPool, uniswapV3Router } from "chain/Tokens.sol";
@@ -21,6 +24,15 @@ contract UniswapAdapterTest {
     /// ███ Storages █████████████████████████████████████████████████████████
 
     IVM vm = IVM(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    VMUtils private utils;
+
+
+    /// ███ Test Setup ███████████████████████████████████████████████████████
+
+    function setUp() public {
+        // Create utils
+        utils = new VMUtils(vm);
+    }
 
 
     /// ███ configure ████████████████████████████████████████████████████████
@@ -148,100 +160,147 @@ contract UniswapAdapterTest {
         require(!adapter.isConfigured(token3), "invalid token3");
     }
 
-//    enum TestType { CallerRepay, CallerNotRepay }
-//
-//    /// @notice Make sure flashSwapWETHForExactTokens is working on Uniswap V2
-//    function testUniswapV2FlashSwapETHForExactTokens() public {
-//        // Create new Uniswap Adapter
-//        UniswapAdapter adapter = new UniswapAdapter(weth);
-//
-//        // Owner set metadata for wbtc
-//        adapter.configure(wbtc, IUniswapAdapter.UniswapVersion.UniswapV2, sushiWBTCETHPair, sushiRouter);
-//
-//        // Execute the flashswap
-//        bytes memory data = abi.encode(TestType.CallerRepay, 2022);
-//        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
-//    }
-//
-//    /// @notice Make sure flashSwapWETHForExactTokens is failed when token is not repay
-//    function testFailUniswapV2FlashSwapETHForExactTokensRevertedIfCallerNotRepay() public {
-//        // Create new Uniswap Adapter
-//        UniswapAdapter adapter = new UniswapAdapter(weth);
-//
-//        // Owner set metadata for wbtc
-//        adapter.configure(wbtc, IUniswapAdapter.UniswapVersion.UniswapV2, sushiWBTCETHPair, sushiRouter);
-//
-//        // Execute the flashswap without repay; this should be reverted
-//        bytes memory data = abi.encode(TestType.CallerNotRepay, 2022);
-//        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
-//    }
-//
-//    /// @notice Make sure flashSwapWETHForExactTokens is working on Uniswap V3
-//    function testUniswapV3FlashSwapETHForExactTokens() public {
-//        // Create new Uniswap Adapter
-//        UniswapAdapter adapter = new UniswapAdapter(weth);
-//
-//        // Owner set metadata for wbtc
-//        adapter.configure(wbtc, IUniswapAdapter.UniswapVersion.UniswapV3, uniswapV3WBTCETHPool, uniswapV3Router);
-//
-//        // Execute the flashswap
-//        bytes memory data = abi.encode(TestType.CallerRepay, 2022);
-//        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
-//    }
-//
-//    /// @notice Make sure flashSwapWETHForExactTokens is failed when token is not repay
-//    function testFailUniswapV3FlashSwapETHForExactTokensRevertedIfCallerNotRepay() public {
-//        // Create new Uniswap Adapter
-//        UniswapAdapter adapter = new UniswapAdapter(weth);
-//
-//        // Owner set metadata for wbtc
-//        adapter.configure(wbtc, IUniswapAdapter.UniswapVersion.UniswapV3, uniswapV3WBTCETHPool, uniswapV3Router);
-//
-//        // Execute the flashswap without repay; this should be reverted
-//        bytes memory data = abi.encode(TestType.CallerNotRepay, 2022);
-//        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
-//    }
-//
-//    /// @notice Check the callback
-//    function onFlashSwapWETHForExactTokens(uint256 _wethAmount, uint256 _amountOut, bytes memory _data) external {
-//        assertGt(_wethAmount, 0, "check _wethAmount");
-//        assertGt(_amountOut, 0, "check _amountOut");
-//        assertEq(IERC20(wbtc).balanceOf(address(this)), _amountOut, "check balance");
-//
-//        // Check passed data
-//        (TestType testType, uint256 pin) = abi.decode(_data, (TestType, uint256));
-//        assertEq(pin, 2022, "check pin");
-//        if (testType == TestType.CallerRepay) {
-//            hevm.setWETHBalance(address(this), _wethAmount);
-//            IERC20(weth).safeTransfer(msg.sender, _wethAmount);
-//            return;
-//        }
-//        if (testType == TestType.CallerNotRepay) {
-//            // Do nothing; this should be reverted
-//            return;
-//        }
-//    }
-//
-//
-//
-//    /// @notice Make sure flashSwapWETHForExactTokens revert if the token is not configured
-//    function testFailFlashSwapETHForExactTokensRevertIfTokenIsNotConfigured() public {
-//        // Create new Uniswap Adapter
-//        UniswapAdapter adapter = new UniswapAdapter(weth);
-//
-//        // Call the flash swap
-//        adapter.flashSwapWETHForExactTokens(hevm.addr(1), 1 ether, bytes(""));
-//    }
-//
-//    /// @notice Make sure swapExactTokensForWETH revert if the token is not configured
-//    function testFailSwapExactTokensForWETHRevertIfTokenIsNotConfigured() public {
-//        // Create new Uniswap Adapter
-//        UniswapAdapter adapter = new UniswapAdapter(weth);
-//
-//        // Call the flash swap
-//        adapter.swapExactTokensForWETH(hevm.addr(1), 1 ether, 1 ether);
-//    }
-//
+
+    /// ███ flashSwapWETHForExactTokens ██████████████████████████████████████
+
+    /// @notice Make sure it reverts if _amountOut is zero
+    function testFlashSwapWETHForExactTokensRevertIfAmountOutZero() public {
+        // Create adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Expect call to revert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IUniswapAdapter.InvalidAmount.selector,
+                0
+            )
+        );
+        adapter.flashSwapWETHForExactTokens(
+            wbtc,
+            0,
+            bytes("")
+        );
+    }
+
+    /// @notice Make sure it reverts if _tokenOut is not supported
+    function testFlashSwapWETHForExactTokensRevertIfTokenOutNotSupported() public {
+        // Create adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Expect call to revert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IUniswapAdapter.TokenNotConfigured.selector,
+                wbtc
+            )
+        );
+        adapter.flashSwapWETHForExactTokens(
+            wbtc,
+            1e8, // 1 WBTC
+            bytes("")
+        );
+    }
+
+    /// Utilities to test the flash swap
+
+    enum TestType { CallerRepay, CallerNotRepay }
+
+    function onFlashSwapWETHForExactTokens(
+        uint256 _wethAmount,
+        uint256 _amountOut,
+        bytes memory _data
+    ) external {
+        // Make sure amount if correct
+        require(_wethAmount > 0, "invalid _wethAmount");
+        require(_amountOut > 0, "invalid _amountOut");
+
+        // Make sure tokenOut is transfered to the flash swap caller
+        uint256 balance = IERC20(wbtc).balanceOf(address(this));
+        require(balance == _amountOut, "invalid balance");
+
+        // Make sure _data is transfered properly
+        (TestType testType, uint256 pin) = abi.decode(_data, (TestType, uint256));
+
+        // Make sure data encoded properly
+        require(pin == 2022, "invalid pin");
+
+        if (testType == TestType.CallerRepay) {
+            utils.setWETHBalance(address(this), _wethAmount);
+            IERC20(weth).transfer(msg.sender, _wethAmount);
+            return;
+        }
+        if (testType == TestType.CallerNotRepay) {
+            // Do nothing; this should be reverted
+            return;
+        }
+    }
+
+    /// @notice Make sure it reverts when caller not repay
+    function testFlashSwapWETHForExactTokensRevertIfCallerNotRepay() public {
+        // Create adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Configure token
+        adapter.configure(
+            wbtc,
+            IUniswapAdapter.UniswapVersion.UniswapV3,
+            uniswapV3WBTCETHPool,
+            uniswapV3Router
+        );
+
+        // Test out the repay
+        bytes memory data = abi.encode(TestType.CallerNotRepay, 2022);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IUniswapAdapter.CallerNotRepay.selector
+            )
+        );
+        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
+    }
+
+
+    /// @notice Make sure flashSwapWETHForExactTokens is working on Uniswap V2
+    function testFlashSwapWETHForExactTokensUniV2() public {
+        // Create new Uniswap Adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Owner set metadata for wbtc
+        adapter.configure(
+            wbtc,
+            IUniswapAdapter.UniswapVersion.UniswapV2,
+            sushiWBTCETHPair,
+            sushiRouter
+        );
+
+        // Execute the flashswap
+        bytes memory data = abi.encode(TestType.CallerRepay, 2022);
+        // This will revert if flash swap failed
+        // see: onFlashSwapWETHForExactTokens
+        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
+    }
+
+
+
+    /// @notice Make sure flashSwapWETHForExactTokens is working on Uniswap V3
+    function testFlashSwapWETHForExactTokensUniV3() public {
+        // Create new Uniswap Adapter
+        UniswapAdapter adapter = new UniswapAdapter(weth);
+
+        // Owner set metadata for wbtc
+        adapter.configure(
+            wbtc,
+            IUniswapAdapter.UniswapVersion.UniswapV3,
+            uniswapV3WBTCETHPool,
+            uniswapV3Router
+        );
+
+        // Execute the flashswap
+        bytes memory data = abi.encode(TestType.CallerRepay, 2022);
+        // This will revert if flash swap failed
+        // see: onFlashSwapWETHForExactTokens
+        adapter.flashSwapWETHForExactTokens(wbtc, 1e8, data);
+    }
+
 //    /// @notice Make sure the its working properly on Uniswap V2
 //    function testUniswapV2SwapExactTokensForWETH() public {
 //        // Create new Uniswap Adapter
